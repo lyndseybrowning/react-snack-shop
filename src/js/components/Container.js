4e89189d79cbda626f1f2169789df3d14db7a744
Lyndsey Browning <lbrowning86@gmail.com>
2016-04-11 20:00:30 +0100

    Added Container component

--- src/js/components/Container.js
+++ src/js/components/Container.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Snack from './Snack';
+import ShoppingCart from './ShoppingCart';
 
-export default React.createClass({
+const Container = React.createClass({
 
-  
-
+  render() {
+    return (
+      <div>
+        <Snack name='Chips' />
+        <Snack name='Cupcake' />
+        <Snack name='Donut' />
+        <Snack name='Doritos' />
+        <Snack name='Popcorn' />
+        <ShoppingCart />
+      </div>
+    );
+  }
 });
+
+export default DragDropContext(Html5Backend)(Container);
