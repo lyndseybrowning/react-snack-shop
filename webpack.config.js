02ec548c5204cf2c97cc82f0810657eb71eb5a55
Lyndsey Browning <lbrowning86@gmail.com>
2016-04-11 19:47:54 +0100

    Boilerplate set up

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+
+/*
+ * Default webpack configuration for development
+ */
+var config = {
+  devtool: 'eval-source-map',
+  entry:  __dirname + "/src/js/main.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  },
+
+  postcss: [
+    require('autoprefixer')
+  ],
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  devServer: {
+    colors: true,
+    historyApiFallback: true,
+    post: process.env.PORT||8080,
+    inline: true,
+    hot: true
+  },
+}
+
+module.exports = config;
